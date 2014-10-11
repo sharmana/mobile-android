63f8c304f095d299846da02f95f65a596dce25ff
Mikhail Maslov <nonatomicgames@gmail.com>
2014-10-11 17:21:53 +0400

    Activities, res-s, updated build.gradle

---
+++ android/src/main/java/com/sharmana/utils/HttpUtils.java
@@ -0,0 +1,15 @@
+package com.sharmana.utils;
+
+/**
+ * Created by MandM on 11.10.2014.
+ */
+public class HttpUtils {
+
+    public static final String DOMAIN = "api.sharmana.ru/";
+    public static final String OK_PATH = "";
+
+    public static String getOkPath() {
+        return DOMAIN + OK_PATH;
+    }
+
+}
