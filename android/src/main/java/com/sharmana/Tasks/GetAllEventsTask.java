651bb9053d539afc2764d5710e9902cd1755ecdc
SergeyTrusov <s.a.trusov@ya.ru>
2014-10-12 03:23:54 +0400

    Added Get All Events

--- android/src/main/java/com/sharmana/Tasks/GetAllEventsTask.java
+++ android/src/main/java/com/sharmana/Tasks/GetAllEventsTask.java
@@ -2,26 +2,30 @@ package com.sharmana.Tasks;
 
 import android.os.AsyncTask;
 import android.util.Log;
+import com.sharmana.db.dto.EventDTO;
+import com.sharmana.db.dto.EventsDTO;
 import com.sharmana.db.dto.UserDTO;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
-
-import java.io.IOException;
 import org.apache.http.util.EntityUtils;
 import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by strusov on 11.10.2014.
  */
-public class SharmanaAuthByYaTokenTask extends AsyncTask<String, Integer, UserDTO> {
+public class GetAllEventsTask extends AsyncTask<String, Integer, EventsDTO> {
 
-    private static final String LOG_TAG = "com.sharmana.Tasks.YandexOAuthTokenTask";
+    private static final String LOG_TAG = "com.sharmana.Tasks.GetAllEventsTask";
 
     OnObjectDtoLoadedListnerer onObjectDtoLoadedListnerer;
 
-    public SharmanaAuthByYaTokenTask(OnObjectDtoLoadedListnerer onObjectDtoLoadedListnerer) {
+    public GetAllEventsTask(OnObjectDtoLoadedListnerer onObjectDtoLoadedListnerer) {
         this.onObjectDtoLoadedListnerer = onObjectDtoLoadedListnerer;
     }
 
@@ -31,29 +35,29 @@ public class SharmanaAuthByYaTokenTask extends AsyncTask<String, Integer, UserDT
     }
 
     @Override
-    protected UserDTO doInBackground(String... params) {
+    protected EventsDTO doInBackground(String... params) {
 
         HttpClient httpclient = new DefaultHttpClient();
-        UserDTO userDTO = null;
+        EventsDTO eventDTO = null;
         try {
             String token = params[0];
-            HttpPost httpPost = new HttpPost("http://api.sharmana.ru/user/auth");
-            httpPost.setHeader("Yandex-Token", token);
+            HttpPost httpPost = new HttpPost("http://api.sharmana.ru/event/my");
+            httpPost.setHeader("Authorization", token);
             HttpResponse response = httpclient.execute(httpPost);
             if(response.getStatusLine().getStatusCode() == 200) {
                 ObjectMapper mapper = new ObjectMapper();
-                userDTO = mapper.readValue(EntityUtils.toString(response.getEntity()), UserDTO.class);
-                Log.i(LOG_TAG, userDTO.get_id());
+                eventDTO = mapper.readValue(EntityUtils.toString(response.getEntity()), EventsDTO.class);
+                Log.i(LOG_TAG, eventDTO.toString());
 
             }
         } catch (IOException e) {
             Log.e(LOG_TAG, e.getMessage());
         }
-        return userDTO;
+        return eventDTO;
     }
 
     @Override
-    protected void onPostExecute(UserDTO result) {
+    protected void onPostExecute(EventsDTO result) {
         //super.onPostExecute(result);
         onObjectDtoLoadedListnerer.objectLoaded(result);
     }
---
+++ android/src/main/java/com/sharmana/db/dto/EventsDTO.java
@@ -0,0 +1,9 @@
+package com.sharmana.db.dto;
+
+import java.util.ArrayList;
+
+/**
+ * Created by strusov on 12.10.2014.
+ */
+public class EventsDTO extends ArrayList<EventsDTO> {
+}
